c5a859eb96747468d7b165cd11e233ac7d09944c
Akash Kumar <40720563+akashkumar59200@users.noreply.github.com>
2020-06-08 16:24:26 +0530

    Add files via upload

---
+++ jqry.js
@@ -0,0 +1,281 @@
+
+$("#sec2").css('margin-top', $("#info").height());
+
+function te1(){
+  console.log("d");
+    $("#g").show();
+    $("#g1").hide();
+     $("#eff").removeClass("effect");
+};
+
+
+   function te2(){
+    $("#g").hide();
+    $("#g1").show();
+    $("#eff").addClass("effect");
+  };
+
+  function tell1(){
+    $("#ba").show();
+    $("#ba1").hide();
+     $("#effe").removeClass("effect");
+     $("#sec2").removeClass("heht1");
+     $("#sec2").addClass("heht2");
+  };
+
+  function tell2(){
+     $("#ba").hide();
+    $("#ba1").show();
+    $("#effe").addClass("effect");
+    $("#sec2").removeClass("heht2");
+    $("#sec2").addClass("heht1");
+  };
+
+  function tell3(){
+     $("#bb").show();
+    $("#bb1").hide();
+     $("#effe1").removeClass("effect");
+     $("#sec2").removeClass("heht1");
+     $("#sec2").addClass("heht");
+  };
+
+  function tell4(){
+     $("#bb").hide();
+    $("#bb1").show();
+    $("#effe1").addClass("effect");
+    $("#sec2").removeClass("heht");
+    $("#sec2").addClass("heht1");
+  };
+
+  function tell5(){
+     $("#bc").show();
+    $("#bc1").hide();
+     $("#effe2").removeClass("effect");
+     $("#sec2").removeClass("heht1");
+     $("#sec2").addClass("heht");
+  };
+
+  function tell6(){
+    $("#bc").hide();
+    $("#bc1").show();
+    $("#effe2").addClass("effect");
+    $("#sec2").removeClass("heht");
+    $("#sec2").addClass("heht1");
+  };
+
+  function tell7(){
+    $("#bd").show();
+    $("#bd1").hide();
+     $("#effe3").removeClass("effect");
+     $("#sec2").removeClass("heht1");
+     $("#sec2").addClass("heht2");
+  };
+
+  function tell8(){
+     $("#bd").hide();
+    $("#bd1").show();
+    $("#effe3").addClass("effect");
+    $("#sec2").removeClass("heht2");
+    $("#sec2").addClass("heht1");
+  };
+
+  function tell9(){
+    $("#be").show();
+    $("#be1").hide();
+     $("#effe4").removeClass("effect");
+     $("#sec3").removeClass("heht1");
+     $("#sec3").addClass("heht");
+  };
+
+  function tell10(){
+    $("#be").hide();
+    $("#be1").show();
+    $("#effe4").addClass("effect");
+    $("#sec3").removeClass("heht");
+    $("#sec3").addClass("heht1");
+  };
+
+  function tell11(){
+    $("#bf").show();
+    $("#bf1").hide();
+     $("#effe5").removeClass("effect");
+     $("#sec3").removeClass("heht1");
+     $("#sec3").addClass("heht");
+  };
+
+  function tell12(){
+    $("#bf").hide();
+    $("#bf1").show();
+    $("#effe5").addClass("effect");
+    $("#sec3").removeClass("heht");
+    $("#sec3").addClass("heht1");
+
+  };
+
+  function tell13(){
+     $("#bh").show();
+    $("#bh1").hide();
+     $("#effe6").removeClass("effect");
+     $("#sec3").removeClass("heht1");
+     $("#sec3").addClass("heht");
+
+  };
+
+  function tell14(){
+    $("#bh").hide();
+    $("#bh1").show();
+    $("#effe6").addClass("effect");
+    $("#sec3").removeClass("heht");
+    $("#sec3").addClass("heht1");
+
+  };
+
+  function tell15(){
+    $("#bi").show();
+    $("#bi1").hide();
+     $("#effe7").removeClass("effect");
+     $("#sec3").removeClass("heht1");
+     $("#sec3").addClass("heht2");
+  };
+
+  function tell16(){
+    $("#bi").hide();
+    $("#bi1").show();
+    $("#effe7").addClass("effect");
+    $("#sec3").removeClass("heht2");
+    $("#sec3").addClass("heht1");
+
+  };
+
+  function tell17(){
+     $("#bj").show();
+    $("#bj1").hide();
+     $("#effe8").removeClass("effect");
+     $("#footer").removeClass("heht1");
+     $("#footer").addClass("heht2");
+
+  };
+
+  function tell18(){
+     $("#bj").hide();
+    $("#bj1").show();
+    $("#effe8").addClass("effect");
+    $("#footer").removeClass("heht2");
+    $("#footer").addClass("heht1");
+  };
+
+  function tell19(){
+     $("#bk").show();
+    $("#bk1").hide();
+     $("#effe9").removeClass("effect");
+     $("#footer").removeClass("heht1");
+     $("#footer").addClass("heht2");
+  };
+
+  function tell20(){
+    $("#bk").hide();
+    $("#bk1").show();
+    $("#effe9").addClass("effect");
+    $("#footer").removeClass("heht2");
+    $("#footer").addClass("heht1");
+  };
+
+  function tell21(){
+    $("#bl").show();
+    $("#bl1").hide();
+     $("#effe10").removeClass("effect");
+     $("#footer").removeClass("heht1");
+     $("#footer").addClass("heht");
+  };
+
+  function tell22(){
+    $("#bl").hide();
+    $("#bl1").show();
+    $("#effe10").addClass("effect");
+    $("#footer").removeClass("heht");
+    $("#footer").addClass("heht1");
+  };
+
+  function tell23(){
+     $("#bm").show();
+    $("#bm1").hide();
+     $("#effe11").removeClass("effect");
+     $("#footer").removeClass("heht1");
+     $("#footer").addClass("heht");
+  };
+
+  function tell24(){
+     $("#bm").hide();
+    $("#bm1").show();
+    $("#effe11").addClass("effect");
+    $("#footer").removeClass("heht");
+    $("#footer").addClass("heht1");
+  };
+
+  function tell25(){
+      $("#bn").show();
+    $("#bn1").hide();
+     $("#effe12").removeClass("effect");
+     $("#footer").removeClass("heht1");
+     $("#footer").addClass("heht");
+  };
+
+  function tell26(){
+    $("#bn").hide();
+    $("#bn1").show();
+    $("#effe12").addClass("effect");
+    $("#footer").removeClass("heht");
+    $("#footer").addClass("heht1");
+  };
+
+  function tell27(){
+    $("#bo").show();
+    $("#bo1").hide();
+     $("#effe13").removeClass("effect");
+     $("#footer").removeClass("heht1");
+     $("#footer").addClass("heht");
+  };
+
+  function tell28(){
+    $("#bo").hide();
+    $("#bo1").show();
+    $("#effe13").addClass("effect");
+    $("#footer").removeClass("heht");
+    $("#footer").addClass("heht1");
+  };
+
+  function tell29(){
+    $("#bp").show();
+    $("#bp1").hide();
+     $("#effe14").removeClass("effect");
+     $("#footer").removeClass("heht1");
+     $("#footer").addClass("heht");
+  };
+
+  function tell30(){
+    $("#bp").hide();
+    $("#bp1").show();
+    $("#effe14").addClass("effect");
+    $("#footer").removeClass("heht");
+    $("#footer").addClass("heht1");
+  };
+
+  function tell31(){
+    $("#bq").show();
+    $("#bq1").hide();
+     $("#effe15").removeClass("effect");
+     $("#footer").removeClass("heht1");
+     $("#footer").addClass("heht");  
+   };
+
+  function tell32(){
+    $("#bq").hide();
+    $("#bq1").show();
+    $("#effe15").addClass("effect");
+    $("#footer").removeClass("heht");
+    $("#footer").addClass("heht1");
+  };
+
+
+
+
